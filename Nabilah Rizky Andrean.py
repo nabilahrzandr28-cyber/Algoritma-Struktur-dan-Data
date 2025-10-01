0ea8802c0250698e1ddb4dc815364f527b7ff3d1
nabilahrzandr28-cyber <nabilahrzandr28@gmail.com>
2025-10-01 12:21:01 +0700

    Add files via upload
    TUGAS KE 2

---
+++ Nabilah Rizky Andrean.py
@@ -0,0 +1,38 @@
+nama='Nabilah Rizky Andrean' 
+usia=19
+hobi= 'Menyanyi dan Berenang' 
+print('nama:',nama) 
+print('usia:',usia) 
+print('hobi:',hobi)
+a,b,c=1,2,'hallo semua'
+d=e=f=10
+g='jadilah yang terbaik'
+print('a:',a)
+print('b:',b)
+print('c:',c)
+print('d:',d)
+print('e:',e)
+print('f:',f)
+print('g:',g)
+nama_depan="nabilah" 
+nama_belakang="rizky andrean" 
+nama_lengkap=nama_depan+nama_belakang
+usia="19"
+print(nama_lengkap);print('usia',usia)
+i=5
+o=10
+print(p)
+nama='nabilah rizky andrean'
+usia=19
+sudah_menikah=False
+print('nama',nama)
+print('usia',usia)
+print('sudah_menikah',sudah_menikah)
+panjang=5
+lebar=10
+luas=panjang*lebar
+print(luas)
+saya_suka_berenang=True
+saya_suka_futsal=False
+print('apakah saya suka berenang',saya_suka_berenang)
+print('apakah saya suka futsal',saya_futsal)
