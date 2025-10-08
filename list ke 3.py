0c38045e63ff6420c6e19b4f4163406cba9371af
nabilahrzandr28-cyber <nabilahrzandr28@gmail.com>
2025-10-08 11:14:53 +0700

    Add files via upload
    tentang list data Nabilah Rizky Andrean

---
+++ list ke 3.py
@@ -0,0 +1,11 @@
+list_buah = ['Pisang', 'Nanas','Melon', 'Durian']
+print(list_buah)
+#hapus item pertama dengan nilai 'Jambu'
+list_buah.remove('Pisang')
+print(list_buah)
+list_buah = ['Mangga', 'Jambu', 'Jeruk', 'Jambu']
+print(list_buah)
+del list_buah[1]
+print(list_buah)
+del list_buah[0:2]
+print(list_buah)
