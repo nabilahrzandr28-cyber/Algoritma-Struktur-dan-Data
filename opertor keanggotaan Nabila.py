7172c678ffe54269b909d1fdb46983798eb456fc
nabilahrzandr28-cyber <nabilahrzandr28@gmail.com>
2025-10-23 11:12:28 +0700

    Add files via upload
    operator dan input

---
+++ opertor keanggotaan Nabila.py
@@ -0,0 +1,26 @@
+ perusahaan = 'Microsoft'
+list_pulau = ['Jawa', 'Sumatra', 'Sulawesi']
+
+# ini adalah dictionary, insyaallah akan kita pelajari
+# di pertemuan-pertemuan yang akan datang
+mahasiswa = {
+    'nama': 'Nabilah Rizky Andrean',
+    'asal': 'Pangkalpinang',
+}
+
+print(
+    "apakah 'c' ada di variabel perusahaan?",
+    'c' in perusahaan
+)
+print(
+    "apakah 'z' ada di variabel perusahaan?",
+    'c' not in perusahaan
+)
+print(
+    "apakah 'Madura' tidak ada di variabel list_pulau?", 
+    'Madura' not in perusahaan
+)
+print(
+    "apakah atribut 'nama' ada di variabel mahasiswa?",
+    'nama' in mahasiswa
+)
