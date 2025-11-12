c6b6461cbb4e8cc604ce86aefa1636a49ec57ffe
nabilahrzandr28-cyber <nabilahrzandr28@gmail.com>
2025-11-12 12:48:36 +0700

    Add files via upload

---
+++ buah_yang_tersedia = ['Jeruk', 'Mangga',.py
@@ -0,0 +1,6 @@
+buah_yang_tersedia = ['Jeruk','Mangga','Melon']
+buah_yang_dicari = input('Masukan nama buah dalam huruf kecil:')
+if(buah_yang_dicari in buah_yang_tersedia):
+    print('buah yaqng dicari tersedia!')
+else:
+    print('buah yang anda cari tidak tersedia')
