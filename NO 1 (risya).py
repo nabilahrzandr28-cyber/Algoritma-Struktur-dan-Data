aaf0e290a316a016012e7f86d10a149b4a5a2953
nabilahrzandr28-cyber <nabilahrzandr28@gmail.com>
2026-01-07 23:13:07 +0700

    Add files via upload

---
+++ NO 1 (risya).py
@@ -0,0 +1,12 @@
+nama = input("Nabilah Rizky Andrean: ")
+usia = input("19: ")
+alamat = input("Bukit Sari: ")
+hobi = input("Bernyanyi: ")
+
+print(f"Nama    : (Nabilah Rizky Andrean)")
+print(f"Usia    : (19) tahun")
+print(f"Alamat  : (Bukit Sari)")
+print(f"Hobi    : (Bernyanyi)")
+
+# Menjalankan fungsi utama
+# tampilkan_biodata()
